1a4ed90cf701b22419c3cb16dd126706c32b9459
Seva <99525875+Tenshi-X@users.noreply.github.com>
2025-09-22 23:56:12 +0700

    Update api.ts

--- amdk-airku-frontend/src/services/api.ts
+++ amdk-airku-frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3001/api', // Adjust if your backend runs on a different port
+  baseURL: 'https://ku-airku-production.up.railway.app/api', // Adjust if your backend runs on a different port
   headers: {
     'Content-Type': 'application/json',
   },
