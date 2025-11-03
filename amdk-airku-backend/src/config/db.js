091b7902b233961c4faf6b683ce248f5f33ab923
Seva <99525875+Tenshi-X@users.noreply.github.com>
2025-11-03 22:26:33 +0700

    Update db.js

--- amdk-airku-backend/src/config/db.js
+++ amdk-airku-backend/src/config/db.js
@@ -2,11 +2,11 @@ const mysql = require('mysql2');
 require('dotenv').config();
 
 const dbConfig = {
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+  host: process.env.MYSQLHOST,
+  port: process.env.MYSQLPORT,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
